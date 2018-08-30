0c3803ab849426ae89a8a13e3bd306dd61e1636e
Felipe Guimaraes <guima.felipec@gmail.com>
2018-08-29 23:28:00 -0300

    added lobby buttons update

--- src/gameobjects/lobbyButton.js
+++ src/gameobjects/lobbyButton.js
@@ -1,6 +1,6 @@
 export default class LobbyButton extends Phaser.GameObjects.Text {
     constructor(scene, x, y, player_name, socket_id){
-        let style = {font:"15px sans-serif", fill:"#ffffff"}
+        let style = {font:"15px sans-serif", fill:"#000000"}
         super(scene, x, y, player_name, style)
         this.normal_tint = 0x000000
         this.selected_tint = 0x550000
--- src/scenes/lobby.js
+++ src/scenes/lobby.js
@@ -4,23 +4,33 @@ export default class Lobby extends Phaser.Scene {
     constructor () {
         super({key: "Lobby"});
     }
+    
+    updateButtons(){
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas", 1));
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas53", 12));
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas4", 11));
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas3", 13));
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas22", 14));
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas2", 15));
+        this.buttons.add(new LobbyButton(this, 0, 0, "Douglas1", 16));
+        let i = 0, j = 0
+        let snapv = 30, snaph = 150
+        let maxLines = 5
+        this.buttons.iterate(button => {
+            button.x = i*snaph;
+            button.y = snapv*j++;
+            if(j == maxLines){
+                j = 0;
+                i++;
+            }
+        })
+    }
 
     create(){
         this.text = this.add.text(0,0,"Lobby:", {font:"40px sans-serif", fill:"#000000"});
 
-        this.buttons = this.add.container(100, 150)
-        let text3 = new LobbyButton(this, 300, 300, 'Beyblade', 19)
-        this.buttons.add(text3, true)
-        text3 = new LobbyButton(this, 300, 400, 'Gugu', 19)
-        this.buttons.add(text3, true)
-        text3 = new LobbyButton(this, 300, 500, 'Riva', 19)
-        this.buttons.add(text3, true)
-        text3 = new LobbyButton(this, 300, 600, 'Gru', 19)
-        this.buttons.add(text3, true)
-
-        // this.buttons.align(-1, 4, 48, 48)
-        // this.buttons.x = 50
-        // this.buttons.y = 100
+        this.buttons = this.add.container(20, 100)
+        this.updateButtons()
 
         this.key_1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
     }
