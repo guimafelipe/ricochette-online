3e4ab11d6f78da574ce9a2e3d969154e7bd6d27d
Felipe Guimaraes <guima.felipec@gmail.com>
2018-11-15 00:09:37 -0200

    implemented enter and exit handles

--- src/gameobjects/menuButton.js
+++ src/gameobjects/menuButton.js
@@ -1,7 +1,7 @@
-export default class LobbyButton extends Phaser.GameObjects.Text {
-    constructor(scene, x, y, player_name, socket_id){
-        let style = {font:"15px sans-serif", fill:"#000000"}
-        super(scene, x, y, player_name, style)
+export default class MenuButton extends Phaser.GameObjects.Text {
+    constructor(scene, x, y){
+        let style = {font:"25px sans-serif", fill:"#000000"}
+        super(scene, x, y, "Olar", style)
         this.normal_tint = 0x000000
         this.selected_tint = 0x550000
         this.hover_tint = 0x3f3f3f
--- src/scenes/lobby.js
+++ src/scenes/lobby.js
@@ -1,4 +1,5 @@
 import LobbyButton from '../gameobjects/lobbyButton'
+import MenuButton from '../gameobjects/menuButton'
 
 export default class Lobby extends Phaser.Scene {
     constructor () {
@@ -16,8 +17,11 @@ export default class Lobby extends Phaser.Scene {
 
         this.buttons.removeAll(true);
 
+        let imInQueue = false;
+
         for(let id in queueUsers){
             this.buttons.add(new LobbyButton(this, 0, 0, queueUsers[id], id));
+            if(id == this.socket.id) imInQueue = true;
         }
 
         let i = 0, j = 0
@@ -30,25 +34,52 @@ export default class Lobby extends Phaser.Scene {
                 j = 0;
                 i++;
             }
-        })
+        });
+
+        if(!imInQueue){
+            this.queueButton.updateText('Enter')
+            this.queueButton.action = this.enterQueueHandle;
+            console.log('enter')
+        } else {
+            this.queueButton.updateText('Exit')
+            console.log('exit')
+            this.queueButton.action = this.exitQueueHandle;
+        }
     }
 
     setupSocket(){
         this.socket.on("lobbyUpdated", queueUsers => {
             this.updateButtons(queueUsers);
-        })
+        });
+    }
+
+    enterQueueHandle(){
+        this.socket.emit('enterQueue');
+    }
+
+    exitQueueHandle(){
+        this.socket.emit('exitQueue');
+    }
+
+    changeNameHandle(name){
+        this.socket.emit('setName', name);
     }
 
     init(data){
         this.socket = data.socket;
+        this.enterQueueHandle = this.enterQueueHandle.bind(this)
+        this.exitQueueHandle = this.exitQueueHandle.bind(this)
+        this.changeNameHandle = this.changeNameHandle.bind(this)
     }
 
     create(){
         this.text = this.add.text(0,0,"Lobby:", {font:"40px sans-serif", fill:"#000000"});
 
-        setupSocket();
+        this.setupSocket();
 
         this.buttons = this.add.container(20, 100)
+        this.queueButton = new MenuButton(this, 180, 180)
+        this.add.existing(this.queueButton)
         this.updateButtons()
 
         this.key_1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
